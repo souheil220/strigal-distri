d8e92d6191c27994cce991529430630c81c0dc34
souheil220 <souhil433@gmail.com>
2021-06-01 09:23:48 +0100

    added schedular for api that brings articles

--- distrubstrugal/jobs/jobs.py
+++ distrubstrugal/jobs/jobs.py
@@ -32,7 +32,8 @@ def schedule_api():
                 la_commande.save()
 
             print('success')
-    except:
+    except Exception as e:
+        print(e)
         print('Error bringing n° odoo')
 
 # to bring new products from odoo
@@ -51,7 +52,37 @@ def schedule_api2():
                 }
         eleme = requests.post(
             "http://10.10.10.64:8585/diststru/prod/", json=data).json()
-    except:
+        for key in eleme.keys():
+            id_article = eleme[key][0]
+            nom_article = eleme[key][1]
+            type_de_categorie = eleme[key][2]
+            categorie_interne = eleme[key][3]
+            famille_article = eleme[key][4]
+            unite_mesure = eleme[key][5]
+            sale_ok = eleme[key][6]
+            type_article = eleme[key][7]
+            template_id = eleme[key][8]
+            company_id = eleme[key][9]
+            active = eleme[key][10]
+            product_id = eleme[key][11]
+            prix_unitaire = eleme[key][17]
+
+            article = Article(id_article=id_article,
+                              nom_article=nom_article,
+                              type_de_categorie=type_de_categorie,
+                              categorie_interne=categorie_interne,
+                              famille_article=famille_article,
+                              unite_mesure=unite_mesure,
+                              sale_ok=sale_ok,
+                              type_article=type_article,
+                              template_id=template_id,
+                              company_id=company_id,
+                              active=active,
+                              product_id=product_id,
+                              prix_unitaire=prix_unitaire)
+            article.save()
+    except Exception as e:
+        print(e)
         print('Error with bringing new articles')
 
 
@@ -78,5 +109,6 @@ def schedule_api3():
                 la_commande.etat = eleme[key][1]
                 la_commande.save()
             print('success')
-    except:
+    except Exception as e:
+        print(e)
         print('Error bringing Etat')
